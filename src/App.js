b4acabb342282860794f23780124ba35683e69fd
im.bipo <bipinkhatri.ram@gmail.com>
2023-08-13 11:47:05 +0545

    event done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Home from "./Home";
 import Contact from "./component/page/Contact";
 import Project from "./component/page/Project";
 import Event, { EventLoader } from "./component/page/Events/Event";
-import EventDetails, { EventDetailsLoader } from "./component/page/Events/EventDetails";
 import Resources from "./component/page/Resources";
 import Teams, { TeamLoader } from "./component/page/teams/Teams";
 import TeamsDetails, { TeamsDetailsLoader } from "./component/page/teams/TeamsDetails";
@@ -25,17 +24,12 @@ import EventLayout from "./Layouts/EventLayout";
 function App() {
   const bmcCsit = createBrowserRouter(
     createRoutesFromElements(
-      <Route element={<RootNav />}>
+      <Route element={<RootNav />} basemame>
         <Route index element={<Home />} />
         <Route path="events" element={<EventLayout />} >
           <Route index 
           element={<Event/>}
           loader={EventLoader}
-          />
-          <Route 
-          path=":eventNo"
-          element={<EventDetails/>}
-          loader={EventDetailsLoader}
           />
         </Route>
         <Route path="resources" element={<Resources />} />
--- src/component/page/Events/Event.js
+++ src/component/page/Events/Event.js
@@ -1,49 +1,56 @@
 import React from "react";
+import EventHeader from "./EventHeader";
 import { Link, useLoaderData } from "react-router-dom";
-import "./Event.css";
 function Event() {
-  const EventData = useLoaderData();
-  console.log(EventData);
+  const EventData = useLoaderData().slice().reverse();
+
+  const linkDisplay = (data) => {
+    console.log(data);
+    if (data !== ""){
+        return (
+        <Link to={data}>
+        <h6 className="w-fit text-sm font-medium text-secondary pt-2">
+        View
+        <hr className="border-primary relative left-2 w-[70%] border" />
+        </h6>
+        </Link>
+        );
+      }
+  };
   return (
     <>
-      <div className="heroEvent p-10">
-        <h1 className="text-secondary text-center pt-10 font-bold text-3xl">
-          EVENTS
-        </h1>
-        <h2 className="m-auto w-[60%] text-center font-bold my-2 text-primary">
-          ORGANIZED BY
-          <br /> CSIT ASSOCATION OF BMC
-        </h2>
-      </div>
-        <div className="bg-gray-100 mt-0 rounded-xl flex justify-center items-start flex-wrap pt-5 desktop:pt-5">
-          {EventData.map((item) => (
-            <div
+      <EventHeader
+        title="EVENTS"
+        subtitle="ORGANIZED BY CSIT ASSOCATION OF BMC"
+      />
+      <div className="bg-gray-100 mt-0 rounded-xl flex justify-center items-start flex-wrap pt-5 desktop:pt-5">
+        {EventData.map((item) => (
+          <div
             key={item.id}
-            className="bg-white flex flex-wrap justify-start items-center mx-2 w-[80%] tablet:mx-5 tablet:w-[25%] desktop:w-[19%] desktop:mx-5 rounded-lg my-5 cursor-pointer"
-            >
-            <Link to={`${item.id}`}>
-                <img src={item.img} alt={item.title} className="rounded-t-lg" />
-                <div className="w-[100%] flex justify-end ">
-                  <h4 className="bg-green-500 px-2 rounded-lg relative right-1 text-white bottom-8 w-fit">
-                    {item.status}
-                  </h4>
-                </div>
-                <div className="p-5 pt-0">
-                  <h3 className="text-xl font-semibold text-primary">
-                    {item.title}
-                  </h3>
-                  <p className="text-primary text-sm">{item.decs}</p>
-                  <h6 className="w-fit text-sm font-medium text-secondary pt-2">
-                    View
-                    <hr className="border-primary relative left-2 w-[70%] border" />
-                  </h6>
-                </div>
-          </Link>
+            className="bg-white flex flex-wrap justify-start items-center mx-2 w-[80%] tablet:mx-5 tablet:w-[25%] desktop:w-[19%] desktop:mx-5 rounded-lg my-5 "
+          >
+            <img
+              src={item.img}
+              alt={item.title}
+              className="rounded-t-lg min-h-80"
+            />
+            <div className="w-[100%] flex justify-end ">
+              <h4 className="bg-green-500 px-2 rounded-lg relative right-1 text-white bottom-8 w-fit">
+                {item.status}
+              </h4>
             </div>
-          ))}
-        </div>
+            <div className="p-5 pt-0">
+              <h3 className="text-xl font-semibold text-primary">
+                {item.title}
+              </h3>
+              <p className="text-primary text-sm">{item.decs}</p>
+              {linkDisplay(item.formLink)}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-    );
+  );
 }
 
 export default Event;
